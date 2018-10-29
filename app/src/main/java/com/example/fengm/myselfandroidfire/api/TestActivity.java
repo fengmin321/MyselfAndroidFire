681c3cfde99db6225d9876536be5d721e2d30b1c
fengmin321 <738334445@qq.com>
2018-10-29 16:21:24 +0800

    新增的activity

---
+++ app/src/main/java/com/example/fengm/myselfandroidfire/api/TestActivity.java
@@ -0,0 +1,16 @@
+package com.example.fengm.myselfandroidfire.api;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by fengm on 2018/10/29.
+ */
+
+public class TestActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
